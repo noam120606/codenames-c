818a468d0259f791ff6b6f42ae96f3589b70f9a4
chloe <quintonchloe53@gmail.com>
2026-02-02 16:25:53 +0100

    SDL suite

--- setup.sh
+++ setup.sh
@@ -11,6 +11,26 @@ if [ ! -d "client/SDL" ]; then
     ../configure
     make
     make install
+    cd ../../
+
+    #SDL2 image
+    git clone https://github.com/libsdl-org/SDL_image.git -b SDL2
+    cd SDL_image
+    mkdir build
+    cd build
+    ../configure
+    make
+    make install
+    cd ../../
+
+    #SDL2 ttf
+    git clone https://github.com/libsdl-org/SDL_ttf.git -b SDL2
+    cd SDL_ttf
+    mkdir build
+    cd build
+    ../configure
+    make
+    make install
     cd ../../../
 fi
 
