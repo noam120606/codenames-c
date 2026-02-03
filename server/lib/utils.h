88f23599f50b5c636a5d34c028fc91282b18d721
Mathis <146639033+kaptainepirate@users.noreply.github.com>
2026-02-03 15:45:56 +0100

    prorotype count_words function in utils.h
    Added count_words function declaration to utils.h

--- server/lib/utils.h
+++ server/lib/utils.h
@@ -3,4 +3,8 @@
 
 int randint(int min, int max);
 
+/*Retourne le nombre de mots (lignes non vides) dans le fichier filepath 
+et retourne -1 en cas d'erreur (par exemple si le fichier filepath est introuvable ...)*/
+int count_words(const char *filepath);
+
 #endif // UTILS_H
