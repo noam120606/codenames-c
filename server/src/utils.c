ffa06df202a514ebcba1db1d8f9426b9853521a2
Mathis <146639033+kaptainepirate@users.noreply.github.com>
2026-02-03 15:44:42 +0100

    Add count_words function prototype
    Added a function prototype for count_words to utils.c.

--- server/src/utils.c
+++ server/src/utils.c
@@ -8,3 +8,7 @@ int randint(int min, int max) {
     int range = max - min + 1;
     return min + rand() / (RAND_MAX / range + 1);
 }
+
+/*Retourne le nombre de mots (lignes non vides) dans le fichier filepath 
+et retourne -1 en cas d'erreur (par exemple si le fichier filepath est introuvable ...)*/
+int count_words(const char *filepath);
