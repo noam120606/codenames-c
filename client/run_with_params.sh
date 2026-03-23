4a248072865592c3b76f43196aabe5bcb7aefdea
Romain/WolfGang_PRoxa <romainpiau10@gmail.com>
2026-03-23 14:05:34 +0100

    Lancements paramètrables : ajouts de tâches

---
+++ client/run_with_params.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+PORT=4242
+# Demander à l'utilisateur de saisir l'adresse IP du serveur
+read -p "Entrez l'adresse IP du serveur (par défaut: 127.0.0.1): " SERVER_IP
+SERVER_IP="${SERVER_IP:-127.0.0.1}"
+
+
+SCRIPT_DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
+cd "$SCRIPT_DIR"
+
+make clean
+make
+exec ./build/client -s "$SERVER_IP" -p "$PORT"
